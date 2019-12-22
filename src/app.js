12074a69b40e04503a702e6579ebab2bbe54b238
Luis Villas <shusten@hotmail.com>
2019-12-22 13:27:48 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const http = require("http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Contet-Type", "text/plain");
+    res.end("Olah Mundo\n");
+
+});
+server.listen(port, hostname, () => {
+    console.log(`Servidor em execução em http://${hostname}:${port}/`);
+  });
+  
+        
